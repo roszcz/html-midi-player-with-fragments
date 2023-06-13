5756a485107a7e6bb2c431ddff5b7d9df4ac7fe1
Greg Cerveny <greg.cerveny@gmail.com>
2023-06-13 14:40:26 -0400

    refactor and simplify

--- script.js
+++ script.js
@@ -14,85 +14,63 @@ window.visualizer = document.getElementById('myVisualizer');
 // player.src = midiFileURL;
 // visualizer.src = midiFileURL;
 
-
-async function fragmentMidiFileFromURL(url) {
-  console.log("reading file");
+async function loadMidiFileBufferFromURL(url) {
   try {
     const response = await fetch(url);
     const arrayBuffer = await response.arrayBuffer();
-    window.midiFileBuffer = buffer.Buffer.from(arrayBuffer);
-    
-    window.parsed = parseMidi(midiFileBuffer);
-    window.fragment = parseMidi(midiFileBuffer);
-    
-    fragment.tracks[0] = parsed.tracks[0].slice(0,200);
-        
-    window.output = writeMidi(fragment);
-    window.outputBuffer = buffer.Buffer.from(output);
-    window.outputBlob = new Blob([outputBuffer]);
-    
-    window.outputSequence = await core.blobToNoteSequence(outputBlob);
-    
-    player.noteSequence = outputSequence;
-    visualizer.noteSequence = outputSequence;
+    const midiFileBuffer = buffer.Buffer.from(arrayBuffer);
+    return midiFileBuffer;
   } catch (error) {
-    console.log("error", error)
+    console.log("error", error);
   }
 }
 
-function loadFragmentIndex(fragmentIndex, phraseIndex) {
-  console.log("load fragments", fragmentIndex, phraseIndex)
-  var phrase = data.fragments[fragmentIndex][phraseIndex];
-  loadMidiFragment(phrase.start_note_index, phrase.end_note_index)
+async function updateSequence(midiBuffer, fragmentIndex, variantIndex, player, visualizer) {
+  const sequence = await sequenceFromFragmentIndex(midiBuffer, fragmentIndex, variantIndex);
+  player.noteSequence = sequence;
+  visualizer.noteSequence = sequence;
 }
 
-async function loadMidiFragment(start, end) {
-    window.parsed = parseMidi(midiFileBuffer);
-    window.fragment = parseMidi(midiFileBuffer);
-    
+async function sequenceFromFragmentIndex(midiFileBuffer, fragmentIndex, phraseIndex) {
+  const phrase = data.fragments[fragmentIndex][phraseIndex];
+  const sequence = await sliceMidiFileBufferToSequence(midiFileBuffer, phrase.start_note_index, phrase.end_note_index)
+  return sequence;
+}
+
+async function sliceMidiFileBufferToSequence(midiFileBuffer, start, end) {
+    const parsed = parseMidi(midiFileBuffer);
+    const fragment = parseMidi(midiFileBuffer);
+    window.parsed = parsed;
+
     fragment.tracks[0] = parsed.tracks[0].slice(start,end);
         
-    window.output = writeMidi(fragment);
-    window.outputBuffer = buffer.Buffer.from(output);
-    window.outputBlob = new Blob([outputBuffer]);
+    const output = writeMidi(fragment);
+    const outputBuffer = buffer.Buffer.from(output);
+    const outputBlob = new Blob([outputBuffer]);
+    const outputSequence = await core.blobToNoteSequence(outputBlob);
     
-    // window.outputSequence = await core.blobToNoteSequence(outputBlob);
-    window.outputSequence = await core.blobToNoteSequence(outputBlob);
-    
-    player.noteSequence = outputSequence;
-    visualizer.noteSequence = outputSequence;  
+    return outputSequence;
 }
-
-
-window.fragmentInput = document.getElementById('fragment');
-window.phraseInput = document.getElementById('phrase');
-window.loadbutton = document.getElementById('loadbutton');
-window.datastatus = document.getElementById('datastatus');
-
-// fragmentMidiFileFromURL(midiFileURL);
-
-window.loadFragmentIndex = loadFragmentIndex;
-window.loadMidiFragment = loadMidiFragment;
-
-const response = await fetch(dataURL);
-const data = await response.json();
-window.data = data;
 
 function updateDataStatus(fragmentIndex) {
   datastatus.innerText = `Fragments: 0 - ${data.fragments.length - 1}
 Fragment[${fragmentIndex}]: 0 - ${data.fragments[fragmentIndex].length - 1}`;
 }
 
+const response = await fetch(dataURL);
+const data = await response.json();
+window.data = data;
 
-// fragment = data.fragments[0]
-// phrase = fragment[0]
+window.datastatus = document.getElementById('datastatus');
 
-// loadMidiFragment(phrase.start_note_index, phrase.end_note_index)
+const midiBuffer = await loadMidiFileBufferFromURL(midiFileURL);
+updateSequence(midiBuffer, 0, 0, player, visualizer);
 
-// loadFragmentIndex(0,0);
-// updateDataStatus(0);
+window.fragmentInput = document.getElementById('fragment');
+window.phraseInput = document.getElementById('phrase');
+window.loadbutton = document.getElementById('loadbutton');
 
 loadbutton.addEventListener("click", function () {
-  loadFragmentIndex(fragmentInput.value, phraseInput.value);
   updateDataStatus(fragmentInput.value);
+  updateSequence(midiBuffer, fragmentInput.value, phraseInput.value, player, visualizer);
 })
